43696adc570e21668d7ea008af04a07edcce641f
Toloza Franco <tolozafranco999@gmail.com>
2022-09-10 11:36:15 -0300

    actualizacion desafio

--- desafios/index.js
+++ desafios/index.js
@@ -1,1 +1,21 @@
-console.log();
+function preguntas () {
+    let precio = parseInt (prompt("Por favor, ingrese el precio de su producto"));
+    let cuotas = parseInt (prompt("Ingresa la cantidad de cuotas en la que desea pagar:3, 6 o 12"));
+
+}
+
+function dividir (numeroUno, numeroDos){
+    if (numeroDos === 3 || numeroDos === 6 || numeroDos === 12){
+        let numeros = numeroUno/numeroDos;
+        let decimales = numeros.toFixed(2);
+        alert("El precio a pagar según la cantidad de cuotas es de $" +  decimales);
+    }else{
+        alert("Esta cantidad de cuotas a pagar no esta permitida");
+        
+    }
+}
+
+let precio = parseInt (prompt("Por favor, ingrese el precio de su producto"));
+let cuotas = parseInt (prompt("Ingresa la cantidad de cuotas en la que desea pagar:3, 6 o 12"));
+
+dividir(precio, cuotas);
