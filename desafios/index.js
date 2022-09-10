3cc0061ab3be1c08b651f69e85dc68cf671de1a9
Toloza Franco <tolozafranco999@gmail.com>
2022-09-10 12:39:55 -0300

    Se agrego Iva

--- desafios/index.js
+++ desafios/index.js
@@ -6,16 +6,17 @@ function preguntas () {
 
 function dividir (numeroUno, numeroDos){
     if (numeroDos === 3 || numeroDos === 6 || numeroDos === 12){
-        let numeros = numeroUno/numeroDos;
-        let decimales = numeros.toFixed(2);
-        alert("El precio a pagar según la cantidad de cuotas es de $" +  decimales);
+        
+        let numeros = (numeroUno/numeroDos)*1.21;
+        let precioEnCuotasConIva = numeros.toFixed(2);
+        alert("El precio a pagar según la cantidad de cuotas es de $" +  precioEnCuotasConIva);
     }else{
         alert("Esta cantidad de cuotas a pagar no esta permitida");
         
     }
 }
 
-let precio = parseInt (prompt("Por favor, ingrese el precio de su producto"));
+let precio = parseInt (prompt("Por favor, ingrese el precio de su producto, se le agregara el I.V.A"));
 let cuotas = parseInt (prompt("Ingresa la cantidad de cuotas en la que desea pagar:3, 6 o 12"));
 
 dividir(precio, cuotas);
